a9997063bd92b1cd23d452202dfc41d20b964e5d
anihernandez <daniel.dhernandez@merrcadolibre.com.co>
2021-02-20 15:18:03 -0500

    Business delegate pattern implementation

--- src/main/java/com/danielrubiano/Lab1/service/EmployeeService.java
+++ src/main/java/com/danielrubiano/Lab1/service/EmployeeService.java
@@ -8,10 +8,10 @@ import com.danielrubiano.Lab1.repo.IEmployeeRepo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 
-@Component
+@Service
 public class EmployeeService implements BusinessService {
     @Autowired
     IEmployeeRepo repo;
